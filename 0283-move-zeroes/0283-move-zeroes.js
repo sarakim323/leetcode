77ec3c2919d92727a126676d997b94b94f6c58a2
Sara Kim <sarakim323@gmail.com>
2023-03-12 21:27:12 -0500

    Time: 94 ms (63.93%), Space: 46.6 MB (69.40%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+// I - array of numbers
+// O - all 0s moved to the end of the array
+// C - don't make a copy of the array
+// E - 
+var moveZeroes = function(nums) {
+    if (Array.isArray(nums) === 'false' || nums.length === 0) {
+        return null;
+    }
+    for (let i = nums.length-1; i >= 0; i--) {
+        if (nums[i] === 0) {
+            nums.push(0);
+            nums.splice(i, 1);
+        }
+    }
+    return nums;
+}; // Time complexity: O(n) || Space complexity: O(1);
