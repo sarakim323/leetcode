32e568f73322053051d8014b8df63ccebeb86935
Sara Kim <sarakim323@gmail.com>
2023-03-12 21:29:28 -0500

    Time: 89 ms (79.27%), Space: 46.6 MB (69.40%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -2,10 +2,6 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// I - array of numbers
-// O - all 0s moved to the end of the array
-// C - don't make a copy of the array
-// E - 
 var moveZeroes = function(nums) {
     if (Array.isArray(nums) === 'false' || nums.length === 0) {
         return null;
