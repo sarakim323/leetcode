e70626e4462c5e2841840363e238465ccef4e10e
Sara Kim <sarakim323@gmail.com>
2023-03-13 20:45:59 -0500

    Time: 98 ms (65.40%), Space: 51.5 MB (71.36%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var rotate = function(nums, k) {
+    let length = nums.length;
+    let result = [...nums];
+    
+    for (let i = 0; i < length; i++) {
+        let index = (i + k) % length;
+        nums[index] = result[i];
+    };
+};
