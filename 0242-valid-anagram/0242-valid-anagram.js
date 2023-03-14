d56f16d197774499610d4170838d7f164c596b05
Sara Kim <sarakim323@gmail.com>
2023-03-13 23:55:11 -0500

    Time: 107 ms (30.16%), Space: 48 MB (29.04%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.js
+++ 0242-valid-anagram/0242-valid-anagram.js
@@ -7,21 +7,10 @@
 // O - boolean
 
 var isAnagram = function(s, t) {
-    // both strings have the same length
-    // both strings have the same letters with the same occurrence
-    
-    // if the lengths are not the same, return false
-    // convert the strings into arrays
-    // sort the input strings in alphabetical order
-    // check if the strings are equal to each other
     if (s.length !== t.length) {
         return false;
     }
     let sortedS = s.split('').sort().join('');
     let sortedT = t.split('').sort().join('');
-    if (sortedS === sortedT) {
-        return true;
-    } else {
-        return false;
-    }
+    return sortedS === sortedT;
 };
